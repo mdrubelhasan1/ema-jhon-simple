30231a9b03f7f3bb30ae876ccb81b0c9b642e84d
mdrubelhasan1 <mdrubelhasan360@gmail.comm>
2023-12-03 18:50:08 +0600

    added products

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     
-    'plugin:react/recommended',
+    // 'plugin:react/recommended', 
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
